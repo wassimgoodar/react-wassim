47e3919c736afc9ace148fafc6f82d7ec718c667
wassimgoodar <wassimislandhaze@gmail.com>
2021-03-13 20:03:05 +0400

    2 files modified

--- src/SubmitButton.js
+++ src/SubmitButton.js
@@ -1,13 +1,32 @@
 import React, { Component } from "react";
 
 class WelcomeClass extends Component {
-  render() {
+  constructor(props){
+    super(props);
+
+    this.state =  {
+      count: 0,
+      count2: 89
+    };
+
+    }
+
+ handleClick = () =>  {
+        this.setState((state) => {
+            return {count: state.count + 1};
+        });
+       
+    }
+
+  render(){
+   
     return (
-      <div>
-        <button >Submit </button>
+      <div class="alighCenter">
+        <button onClick = {this.handleClick}>Submit </button>
+        <div id = "output">{this.state.count}</div>
+        <div id = "output2">{this.state.count2}</div>
       </div>
-    );    
+    );
   }
- 
 }
 export default WelcomeClass;
